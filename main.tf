6636e8990c06ee34af7c5ca3b944c52b759f69ed
Giridhar <giri.j.raju@gmail.com>
2016-07-09 15:53:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "us-west-2"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-775e4f16"
+    ami = "ami-2051294a"
     instance_type = "t2.micro"
-    key_name = "hari"
-    subnet_id = "subnet-5018af34"
+    key_name = "devkey"
+    subnet_id = "subnet-62bf7448"
     user_data = "${file("userdata.sh")}"
     tags {
-           Name = "haripc"
+           Name = "SampleJenkins"
        }
   }
